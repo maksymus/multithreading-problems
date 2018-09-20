80eff265f02c62d426c44a4f7392dc8b8bd01641
maksymus@gmail.com <maksymus@gmail.com>
2018-09-19 20:33:06 -0400

    counting semaphore

---
+++ src/main/java/semaphore/CountingSemaphore.java
@@ -0,0 +1,27 @@
+package semaphore;
+
+public class CountingSemaphore {
+
+    int usedPermits = 0; // permits given out
+    int maxCount;  // max permits to give out
+
+    public CountingSemaphore(int count) {
+        this.maxCount = count;
+    }
+
+    public synchronized void acquire() throws InterruptedException {
+        while (usedPermits >= maxCount)
+            wait();
+
+        usedPermits++;
+        notifyAll();
+    }
+
+    public synchronized void release() throws InterruptedException {
+        while (usedPermits == 0)
+            wait();
+
+        usedPermits--;
+        notifyAll();
+    }
+}
